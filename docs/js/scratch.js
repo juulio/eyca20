73708c92c4f05d8c6d89213620958cf9ba25ab7b
Julio Del Valle <juulio@gmail.com>
2022-01-12 20:14:48 -0600

    Adding EYCA assets

--- docs/js/scratch.js
+++ docs/js/scratch.js
@@ -1,9 +1,6 @@
 let sc;
-let imagesArray = ['./img/01.jpg', './img/02.jpg', './img/03.jpg', './img/04.jpg', './img/05.jpg', './img/06.jpg', './img/07.gif',
- './img/08.gif', './img/09.jpg', './img/10.jpg', './img/11.jpg','./img/12.jpg', './img/13.gif','./img/14.gif','./img/15.jpg',
- './img/16.jpg', './img/17.jpg', './img/18.jpg', './img/19.jpg','./img/20.jpg', './img/21.gif','./img/22.gif','./img/23.jpg',
- './img/24.jpg', './img/25.jpg', './img/26.jpg', './img/27.jpg','./img/28.jpg', './img/29.gif','./img/30.gif','./img/31.jpg',
- './img/32.jpg', './img/33.jpg', './img/34.jpg'];
+let imagesArray = ['./img/01.png', './img/02.png', './img/03.png', './img/04.png', './img/05.png', './img/06.png', './img/07.jpg',
+ './img/08.png'];
  
 const imagesArrayLength = imagesArray.length;
 let isMobileDevice = (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
