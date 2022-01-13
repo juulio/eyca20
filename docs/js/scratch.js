f5c70466017c9a305348e290af6afe576e66d861
Julio Del Valle <juulio@gmail.com>
2022-01-13 12:12:12 -0600

    Updatin images

--- docs/js/scratch.js
+++ docs/js/scratch.js
@@ -1,5 +1,5 @@
 let sc;
-let imagesArray = ['./img/01.png', './img/02.png', './img/03.png', './img/04.png', './img/05.png', './img/06.png', './img/07.jpg',
+let imagesArray = ['./img/01.png', './img/02.png', './img/03.png', './img/04.png', './img/05.png', './img/06.jpg', './img/07.jpg',
  './img/08.png'];
  
 const imagesArrayLength = imagesArray.length;
